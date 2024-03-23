da8ab060a4f727a4286c5ed07f479d3ddbff91ac
Oluwatoyin <myworldco@gmail.com>
2024-03-23 13:38:23 +0100

    Smoke Test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+ //Fetch Contract to Deploy
+  const Token = await ethers.getContractFactory("Token")
+
+  //Deploy Contract
+ const token = await Token.deploy()
+ await token.deployed()
+ console.log(`Token Deployed to : ${token.address}`)
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
